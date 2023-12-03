f233beb982c3940a7562152440c82874d4ca1aee
Sinchana P <sinchanap@ivoyant.com>
2023-12-03 08:41:54 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+
+import { Home } from './components/Home';
+import { Navbar } from './components/Navbar';
+import { Products } from './components/Products';
+import { ContactUs } from './components/ContactUs';
+import { NoMatch } from './components/NoMatch';
+import { Mens } from './components/Mens';
+import { Womens } from './components/Womens';
+import { Kids } from './components/Kids';
+import { MensProductDetails } from './components/MensProductDetails';
+import { WomensProductDetails } from './components/WomensProductDetails';
+import { KidsProductDetails } from './components/KidsProductDetails';
+import { Login } from './components/Login';
+import { Profile } from './components/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Navbar />
+    <Routes>
+      <Route path='/' element={ <Home /> }/> 
+
+      <Route path='/products' element={ <Products /> }>
+          <Route index element={ <Mens /> } />
+
+          <Route path='mens' element={ <Mens /> } />
+          <Route path='mens/:mensId' element={ <MensProductDetails /> } />
+
+          <Route path='womens' element={ <Womens /> } />
+          <Route path='womens/:womensId' element={ <WomensProductDetails /> } />
+
+          <Route path='kids' element={ <Kids /> } />
+          <Route path='kids/:kidsId' element={ <KidsProductDetails /> } />
+
+      </Route>
+      <Route path='products/:mensId' element={ <MensProductDetails /> } />
+
+      <Route path='/login' element={ <Login /> } />
+      <Route path='/profile/:name' element={ <Profile /> } />
+
+      <Route path='/contactUs' element={ <ContactUs /> } />
+      <Route path='*' element={ <NoMatch /> } />
+    </Routes>
+   </>
   );
 }
 
